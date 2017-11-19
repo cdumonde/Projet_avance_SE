b85b06d6167f35ede15ab30ec638fa173cc1f9b6
cdumonde <cedric.dumondelle1@gmail.com>
2017-11-19 17:37:11 +0100

    update

--- Cedric_Commit/filtre_correcteur/script_affichage.sce
+++ Cedric_Commit/filtre_correcteur/script_affichage.sce
@@ -3,5 +3,7 @@ clc;
 close;
 offset = 982;
 data = read("data.txt", 4096 + offset, 1);
+data2 = read("filtre.txt", 4096 + offset, 1);
 
-plot(data(offset : length(data)));
+plot(data(offset : length(data)), 'b') ;
+plot(data2(offset : length(data)), 'g');
--- Cedric_Commit/filtre_correcteur/src/main.cpp
+++ Cedric_Commit/filtre_correcteur/src/main.cpp
@@ -1,25 +1,31 @@
 #include "./reader/reader.h"
 #include "./writer/writer.h"
+#include "./passeHaut/passeHaut.h"
 
 #include <iostream>
 
 int main( int argc, char * argv[]){
 
 	cout << "Initialisation des composants..." << endl;
-	Reader	FileReader("File");
-	Writer	PreFiltre("PreFiltre");
+	Reader		FileReader("File");
+	PasseHaut	HighPass("HighPass");
+	Writer		HighPassOutput("HighPassOutput");
 	FileReader.setFileName("data.txt");
-	PreFiltre.setFileName("filtre.txt");
+	HighPassOutput.setFileName("filtre.txt");
 
 	cout << "Creation des signaux d'interconnexion..." << endl;
 	sc_fifo<float>	file_fifo("fileFifo", 5078);
-	sc_fifo<float>	preFilter_fifo("preFilterFifo", 5078);
+	sc_fifo<float>	highPass_fifo("highPassFifo", 5078);
+	sc_fifo<float>	highPassOutput_fifo("highPassOutputFifo", 5078);
 
 	cout << "Mapping des composants..." << endl;
 	FileReader.data_out(file_fifo);
-	PreFiltre.data_in(file_fifo);
+	HighPass.data_in(file_fifo);
 
-	PreFiltre.data_out(preFilter_fifo);
+	HighPass.data_out(highPass_fifo);
+	HighPassOutput.data_in(highPass_fifo);
+
+	HighPassOutput.data_out(highPassOutput_fifo);
 
 	cout << "Lancement de la simulation du circuit..." << endl;
 	sc_start(1000, SC_MS);
